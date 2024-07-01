1e9f82865e56888b4012d06495ac65646213ab24
Marcin Gesek <marcin.gesek79@gmail.com>
2024-07-01 23:30:46 +0200

    Print file's content.

--- osw-interpreter/main.cpp
+++ osw-interpreter/main.cpp
@@ -1,8 +1,35 @@
-﻿#include <fmt/core.h>
+﻿#include <cassert>
+#include <fstream>
+#include <sstream>
+#include <fmt/core.h>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+#define RT(str) fmt::runtime(str) 
+
+static std::string getFileContentAsString(const std::string& path)
 {
-	fmt::println("Hello world!");
+	std::ifstream ifs{ path };
+
+	if (!ifs)
+	{
+		fmt::println("Error: can't open the file: {}", path);
+	}
+	
+	std::stringstream buffer{};
+	buffer << ifs.rdbuf();
+
+	return buffer.str();
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		fmt::println("Error: provide a path to .osw file.");
+		return -1;
+	}
+
+	std::string file_content{ getFileContentAsString(argv[1]) };
+	fmt::println(RT(file_content));
 
 	return 0;
 }
