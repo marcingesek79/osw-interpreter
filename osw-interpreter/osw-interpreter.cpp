e6e508fc6c0c4c9374463bcbf694b576e2914b72
Marcin Gesek <marcin.gesek79@gmail.com>
2024-07-01 19:30:30 +0200

    Add project files.

---
+++ osw-interpreter/osw-interpreter.cpp
@@ -0,0 +1,12 @@
+﻿// osw-interpreter.cpp : Defines the entry point for the application.
+//
+
+#include "osw-interpreter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
